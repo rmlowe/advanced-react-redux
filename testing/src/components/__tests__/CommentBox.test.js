01fc6d729b9a19affd2822e5e91358c96c8c5e49
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-19 18:16:25 +0800

    Providing Fake Events

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,9 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('textarea').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simultate('change', {
+    target: { value: 'new comment' }
+  })
+});
