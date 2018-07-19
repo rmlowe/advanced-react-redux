593b38e8453ccb8389fb714150fc7dc9d9d6b498
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-19 13:58:31 +0800

    Tricking React with JSDOM

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box' () => {
-  
+  const div = document.create('div');
+
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // and checks to see if the CommentBox is in there
+
+  ReactDOM.unmountComponentAtNode(div);
 });
