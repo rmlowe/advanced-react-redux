105f0b6059c1e23550519fbb1a31a29f4a1d0a25
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-26 20:32:50 +0800

    Inspecting the Database

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost/auth');  
 
 // App Setup
 app.use(morgan('combined'));
